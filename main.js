10845d5289b9a9ad9827dd96092ff5d7061d2da3
erin <erin.endrei@gmail.com>
2022-11-09 16:52:12 +1000

    Add displayBook function

--- main.js
+++ main.js
@@ -1,35 +1,42 @@
 let myLibrary = []
 
-let testBook1 = new Book("Miss Garnet's Angel", "Salley Vickers", "305", false)
-let testBook2 = new Book("You Are Gods", "David Bentley Hart", "305", true)
+let testBook1 = new Book("Miss Garnet's Angel", "Salley Vickers", "305", "Not read")
+let testBook2 = new Book("You Are Gods", "David Bentley Hart", "305", "Read")
 
 myLibrary.push(testBook1, testBook2)
 
 const container = document.querySelector(".container")
 
-myLibrary.forEach(item => {
-    console.log(item.info())
-    let card = document.createElement("div");
-    card.textContent = item.info()
-    container.appendChild(card).className = "card";
-});
-
-
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
-    this.pages = pages
+    this.pages = pages + " pages"
     this.read = read
     this.info = function () {
         if (this.read === true) {
             readString = 'read'
         }
         else { readString = 'not read yet' }
-        return `${title} by ${author}, ${pages} pages, ${readString}`
+        return `${title} \n\n by ${author}, ${pages} pages, ${readString}`
     }
 }
 
-function addBookToLibrary() {
 
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read)
+    myLibrary.push(newBook)
 }
 
+myLibrary.forEach(displayBook);
+
+function displayBook(item, index) {
+    let card = document.createElement("div");
+
+    ["title", "author", "pages", "read"].forEach(e => {
+        let element = document.createElement("p");
+        card.appendChild(element)
+        element.textContent = item[e]
+    })
+    card.setAttribute('data-index', index)
+    container.appendChild(card).className = "card";
+}

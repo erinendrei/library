e4c3631cc1322244ed9801035368f372cc31abf1
erin <erin.endrei@gmail.com>
2022-11-09 12:39:46 +1000

    Add function to loop through library array and display book info

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+let myLibrary = []
+
+let testBook1 = new Book("Miss Garnet's Angel", "Salley Vickers", "305", false)
+let testBook2 = new Book("You Are Gods", "David Bentley Hart", "305", true)
+
+myLibrary.push(testBook1, testBook2)
+
+const container = document.querySelector(".container")
+
+myLibrary.forEach(item => {
+    console.log(item.info())
+    let card = document.createElement("div");
+    card.textContent = item.info()
+    container.appendChild(card).className = "card";
+});
+
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -11,3 +28,8 @@ function Book(title, author, pages, read) {
         return `${title} by ${author}, ${pages} pages, ${readString}`
     }
 }
+
+function addBookToLibrary() {
+
+}
+

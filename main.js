aa3890161fdd557ae2333278165d37e0c3b89b9f
erin <erin.endrei@gmail.com>
2022-11-10 16:26:45 +1000

    Implement read/unread toggling

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let myLibrary = []
 
 let testBook1 = new Book("Miss Garnet's Angel", "Salley Vickers", "305", "Not read")
-let testBook2 = new Book("You Are Gods", "David Bentley Hart", "305", "Read")
+let testBook2 = new Book("Truth and Truthfulness", "Bernard Williams", "305", "Read")
 
 myLibrary.push(testBook1, testBook2)
 
@@ -21,6 +21,17 @@ function Book(title, author, pages, read) {
     }
 }
 
+Book.prototype.toggleRead = function () {
+
+    if (this.read === "Read") {
+        this.read = "Not read"
+
+    }
+    else {
+        this.read = "Read"
+    }
+
+}
 
 function addBookToLibrary(title, author, pages, read) {
     let newBook = new Book(title, author, pages, read)
@@ -35,16 +46,26 @@ function displayBook(item, index) {
 
     ["title", "author", "pages", "read"].forEach(e => {
         let element = document.createElement("p");
+        if (e === "read") {
+            element.setAttribute('id', `read_${index}`)
+        }
         card.appendChild(element)
         element.textContent = item[e]
 
     })
+
     card.setAttribute('data-index', index)
+
     let closeCardButton = document.createElement("button")
-    closeCardButton.setAttribute('card_id', index)
     closeCardButton.addEventListener('click', removeBookFromLibrary)
     card.appendChild(closeCardButton)
     closeCardButton.textContent = "Remove"
+
+    let toggleReadButton = document.createElement("button")
+    toggleReadButton.classList.add('toggleRead')
+    card.appendChild(toggleReadButton)
+    toggleReadButton.textContent = "Read"
+
     container.appendChild(card).className = "card";
 }
 
@@ -53,6 +74,7 @@ const newBookForm = document.querySelector("div.formWrapper")
 const formWrapperWrapper = document.querySelector("div.formWrapperWrapper")
 const closeFormButton = document.getElementById("closeForm")
 const submitButton = document.getElementById("submit")
+const toggleReadButtons = document.querySelectorAll("button.toggleRead")
 
 newBookButton.addEventListener('click', activateFormModal)
 closeFormButton.addEventListener('click', closeFormModal)
@@ -84,8 +106,19 @@ function submitForm(e) {
 }
 
 function removeBookFromLibrary(e) {
-    let bookIndex = e.target.getAttribute("card_id")
+    let bookIndex = e.target.parentNode.dataset.index
     myLibrary.splice(bookIndex, 1)
     let card = e.target.parentNode
     card.remove()
+}
+
+
+toggleReadButtons.forEach(btn => btn.addEventListener('click', toggleRead))
+
+function toggleRead(e) {
+    let index = e.target.parentNode.dataset.index
+    let book = myLibrary[index]
+    book.toggleRead()
+    let readText = document.getElementById(`read_${index}`)
+    readText.textContent = book.read
 }
